cadeeec9163f3cf5aeed9e79c5c1b9eee53e8901
roza-karapetyan <rosakarapetian@gmail.com>
2023-06-02 23:47:03 +0300

    lesson_2_task_7.py

--- lesson_2_task_7.py
+++ lesson_2_task_7.py
@@ -1,3 +1,12 @@
+# lst = [11, 5, 8, 32, 15, 3, 20, 132, 21, 4, 555, 9, 20]
+# s = sum(lst)
+# print(s)
+
 lst = [11, 5, 8, 32, 15, 3, 20, 132, 21, 4, 555, 9, 20]
-s = sum(lst)
-print(s)
+l = len(lst)
+var1 = lst[0]
+for i in range(1, l):
+    var1 = var1 + lst[i]
+print(var1)
+  
+
